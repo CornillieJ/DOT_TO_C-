2972d5b393f78a9f5d02906fb8bf4f8e67653ade
Jeffrey Cornillie <jeffrey.cornillie@student.howest.be>
2024-11-12 17:36:27 +0100

    Adjust bat to allow drag from other folder

--- convert.bat
+++ convert.bat
@@ -1,4 +1,12 @@
 @echo off
+REM Save the current directory and change to the batch file's directory
+pushd %~dp0
+
+REM Run the Python script with the dragged file as an argument
 python3 .\Convert.py %1
+
+REM Return to the original directory (optional)
+popd
+
 ECHO Your files are in the output folder
 pause
